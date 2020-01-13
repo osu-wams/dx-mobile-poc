4ef6e14fe988300e3a1af9fa74338e6e0a35d24e
Josh Gum <revgum@gmail.com>
2020-01-13 12:25:49 -0800

    initial commit

---
+++ src/AppStyles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
