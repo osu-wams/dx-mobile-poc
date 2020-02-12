e2106f0f2c796c0075911f45fafa09819691919a
Josh Gum <revgum@gmail.com>
2020-02-12 13:17:33 -0800

    point mobile app to dev cluster

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-const APP_HOST = 'http://local.my.oregonstate.edu:3000';
+const APP_HOST = 'https://dev.my.oregonstate.edu';
 const AUTH_STORE_KEY = 'dx-auth-token';
 
 export { APP_HOST, AUTH_STORE_KEY };
